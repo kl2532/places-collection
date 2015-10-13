98dd20b4a38cb82a926458bd65b7cc8d089d93b4
Katie Lin <kl2532@columbia.edu>
2015-10-13 04:57:07 -0400

    Added validation for form, trying to add error message

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User
   field :first_name, type: String
   field :last_name, type: String
   field :email, type: String
+
+  validates :first_name, :presence => { :message => "You must provide a first name."} 
+  validates :last_name, :presence => { :message => "You must provide a last name."} 
+  validates :email, :presence => { :message => "You must provide an email."} 
 end
